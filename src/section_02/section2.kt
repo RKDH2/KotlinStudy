0cbf21f12d4ea78693049f29a45d5e248da28197
rkdhs <gaons120@gmail.com>
2024-08-25 20:27:16 +0900

    Section2

--- src/section_02/section2.kt
+++ src/section_02/section2.kt
@@ -2,6 +2,12 @@ package section_02
 
 fun main() {
     var a: Int = 12345
-    var b: Long = a.toLong() // 명시적 형변환
+    var b: Long = a.toLong() // 명시적 형 변환
     println(b)
+
+    var nullArray = arrayOfNulls<Int>(5) // 비어있는 배열
+    var intArray = arrayOf(1, 2, 3, 4, 5) // 순서대로 값을 추가
+
+    intArray[1] = 3 // 해당 인덱스에 값을 할당
+    println(intArray[1])
 }
